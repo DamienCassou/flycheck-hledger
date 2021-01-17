8b853d0e41cf7b8f78b1e14ebe99b60e5957b44a
Damien Cassou <damien@cassou.me>
2021-01-17 18:05:14 +0100

    Add initial package documentation.

--- flycheck-hledger.el
+++ flycheck-hledger.el
@@ -22,8 +22,10 @@
 
 ;;; Commentary:
 
-;; Adapted from flycheck-ledger by Steve Purcell
-;; (https://github.com/purcell/flycheck-ledger).
+;; This package is a flycheck [1] checker for hledger files [2].
+;;
+;; [1] https://www.flycheck.org/en/latest/
+;; [2] https://hledger.org/
 
 ;;; Code:
 
