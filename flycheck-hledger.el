f307ec5b594e1f449667dd8280ea15fad12f19e8
Damien Cassou <damien@cassou.me>
2023-06-26 21:27:46 +0200

    Update copyright years

--- flycheck-hledger.el
+++ flycheck-hledger.el
@@ -1,6 +1,6 @@
 ;;; flycheck-hledger.el --- Flycheck module to check hledger journals  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2021  Damien Cassou
+;; Copyright (C) 2020-2023  Damien Cassou
 
 ;; Author: Damien Cassou <damien@cassou.me>
 ;; Url: https://github.com/DamienCassou/flycheck-hledger/
